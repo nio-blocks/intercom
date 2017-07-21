204e25cec2af3ad68efcdecee7b3a884e81c86b8
cowleyk <cowley.kevin.l@gmail.com>
2017-07-21 16:08:47 -0600

    Move user info into an object property, require one or the other to be configured in block

--- intercom_tag_users_block.py
+++ intercom_tag_users_block.py
@@ -1,6 +1,13 @@
 from nio.block.base import Block
-from nio.properties import StringProperty
+from nio.properties import StringProperty, PropertyHolder, ObjectProperty
 import requests
+
+
+class UserInfo(PropertyHolder):
+    email = StringProperty(
+        title="User Email", default="{{ $user }}", allow_none=True)
+    id = StringProperty(
+        title="ID", default="{{ $user_id }}", allow_none=True)
 
 
 class IntercomTagUsers(Block):
@@ -8,15 +15,24 @@ class IntercomTagUsers(Block):
     access_token = StringProperty(
         title="Access Token", default="[[INTERCOM_ACCESS_TOKEN]]")
     tag_name = StringProperty(title="Name of Tag", default="HappyCustomer")
-    email = StringProperty(title="User Email", default="{{ $user }}")
+    user_info = ObjectProperty(UserInfo, title="User Info", default=UserInfo())
 
     def process_signals(self, signals):
         for signal in signals:
-            response = self._request(body={
-                "name": self.tag_name(signal),
-                "users": [{"email": self.email(signal)}],
-            })
-            if response.status_code != 200:
+            if self.user_info().email(signal) or self.user_info().id(signal):
+                response = self._request(body={
+                    "name": self.tag_name(signal),
+                    "users": [
+                        {"email": self.user_info().email(signal)}
+                    ]
+                    if self.user_info().email(signal) else [
+                        {"id": self.user_info().id(signal)}
+                    ]
+                })
+                if response.status_code != 200:
+                    raise Exception
+            else:
+                self.logger.error("No user info configured")
                 raise Exception
 
     def _request(self, body={}):
